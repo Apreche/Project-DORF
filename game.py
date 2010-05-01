0e371ffa354c86a505022a7de27b44e055dbbeec
Alex Jarocha-Ernst <jormungandr@frontrowcrew.com>
2010-05-02 00:55:09 +0800

    Grid is larger than viewport; viewport can be moved with arrow keys

--- game.py
+++ game.py
@@ -8,12 +8,14 @@ from grid import Grid
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Project D.O.R.F.')
-screen.fill((0,0,0))
+
+viewportX = 0
+viewportY = 0
 
 # create the grid, fill it with nodes
 gameGrid = Grid()
-for x in range(0,80):
-    for y in range(0,60):
+for x in range(-160,160):
+    for y in range(-120,120):
         contents = random.choice(string.letters).upper()
         gameGrid.add_node((x, y, 0), contents)
 
@@ -21,17 +23,37 @@ font_file = pygame.font.match_font('freemono')
 font = pygame.font.Font(font_file, 10)
 font.set_bold(True)
 
-for node in gameGrid.nodes():
-    text = font.render(node.contents, 1, (255, 255, 255))
-    rect = text.get_rect()
-    rect.x, rect.y, z = node.location
-    rect.x *= 10
-    rect.y *= 10
-    screen.blit(text, rect)
+#for node in gameGrid.nodes():
+def updateDisplay():
+	screen.fill((0,0,0))
+	for x in range(viewportX, viewportX+80):
+		for y in range(viewportY, viewportY+60):
+			node = gameGrid.get_node_at((x, y, 0))
+			if node != None:
+				text = font.render(node.contents, 1, (255, 255, 255))
+				rect = text.get_rect()
+				rect.x, rect.y, z = node.location
+				rect.x -= viewportX
+				rect.y -= viewportY
+				rect.x *= 10
+				rect.y *= 10
+				screen.blit(text, rect)
+	pygame.display.update()
 
-pygame.display.update()
+updateDisplay()
 
 while True:
-   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-         sys.exit()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_DOWN:
+				viewportY += 1
+			if event.key == pygame.K_UP:
+				viewportY -= 1
+			if event.key == pygame.K_LEFT:
+				viewportX -= 1
+			if event.key == pygame.K_RIGHT:
+				viewportX += 1
+			updateDisplay()
+	
--- grid.py
+++ grid.py
@@ -6,10 +6,18 @@ class Grid(graph):
 
     def __init__(self):
         super(Grid, self).__init__()
+        self.locationNodes = {}
 
     def add_node(self, location, contents=None):
         new_node = Node(location, contents)
+        self.locationNodes[location] = new_node;
         super(Grid, self).add_node(new_node)
+
+    def get_node_at(self, location):
+		if location in self.locationNodes:
+			return self.locationNodes[location];
+		else:
+			return None
 
     def get_node(self, location, contents=None):
         test_node = Node(location, contents)
